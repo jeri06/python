802fec33a5aea70b306d523838de73692acdb1f9
jeri06 <jeriyulfiranda94@gmail.com>
2019-01-20 02:58:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     # tornado.locale.load_translations(settings['locale_path'])
     #port = os.environ['PORT']
     ports = os.environ['PORT']
+    print(ports)
     run_dev(ports)
 
 
